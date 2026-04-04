ce167a255d2957d505910631028241936c9bcfab
Students of 2026 <am12805@DCLAP-V1525-CSD.abudhabi.nyu.edu>
2026-04-04 11:50:02 +0000

    problem with address

--- server.c
+++ server.c
@@ -411,9 +411,9 @@ int main(void)
                 char error_message[BUFFER_SIZE];
 
                 // snprintf(error_message, sizeof(error_message), "Command not found: %s\n", buffer);
-                snprintf(error_message, sizeof(error_message), "Command not found: %.4074s\n", buffer);
-
-                printf("[ERROR] Command not found: \"%s\"\n", buffer);
+               // snprintf(error_message, sizeof(error_message), "Command not found: %.4074s\n", buffer);
+                snprintf(error_message, sizeof(error_message), "Command not found:\n");
+printf("[ERROR] Command not found: \"%s\"\n", buffer);
                 printf("[OUTPUT] Sending error message to client: \"Command not found: %s\"\n", buffer);
 
                 if (send_all(client_fd, error_message, strlen(error_message)) < 0)
